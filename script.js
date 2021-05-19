b0f1ac2d75d04d27cde2307e451b34d16be097a3
Gil Cohen <76738594+gilico@users.noreply.github.com>
2021-05-19 07:25:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+﻿var isCardHidden = true;
+var flipCnt = 0;
+var firsCard = "";
+var secCard = "";
+
+function flipCard(id,name) {
+    var Card = document.getElementById(id);
+    if (id != temp) {
+        Card.style.transform = "rotateY(180deg)";
+        firsCard = name;
+    } 
+    var temp = id;
+    console.log(firsCard);
+}
+
+function backFlip(id, name) {
+    var Card = document.getElementById(id);
+
+    console.log(secCard);
+}
