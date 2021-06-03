a6d066f6d9aaeaba908e968c3ed17101e986de6b
Gil Cohen <76738594+gilico@users.noreply.github.com>
2021-06-03 18:01:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,125 +1,177 @@
-﻿var cardName1;
-var cardName2;
+﻿var deckCard = document.querySelectorAll('.game-card');
+deckCard.forEach(i => i.addEventListener('click', Flipper));
 
-//----CARDS NAME FUNCTION-----//
-function getCardName(name) {
-    if (isCardFlipped) {
-        cardName1 = name;
-        isCardFlipped = true;
-        cardCnt++;
+//---shuffles the cards on load----//
+var cardPosition;
+deckCard.forEach(j => {
+    cardPosition = Math.floor(Math.random() * 16);
+    j.style.order = cardPosition;
+});
+
+
+//---CARDS NAMES FUNCTION----//
+var isCardFlip = false;
+var card1, card2;
+var name1, name2;
+
+function GetCardName(name) {
+    if (isCardFlip) {
+        name1 = name;
     } else {
-        cardName2 = name;
-        isCardFlipped = false;
-        cardCnt++;
+        name2 = name;
     }
 }
 
+//---FLIP 2 CARDS FUNCTION----//
+var isBoardLocked = false;
 
-//----FLIP CARDS FUNCTION-----//
-var isCardFlipped = false;
-var isCardFaceUp = false; //dening other cards to flip while 2 cards facing up
-var isCardsMath = false;
-var card1;
-var card2;
-var deckCard = document.querySelectorAll('.game-card');
-deckCard.forEach(i => i.addEventListener('click', flip));
-
-function flip() {
-    turnDetect();
-    if (isCardFaceUp) {
+function Flipper() {
+    //dening the more card to flip
+    if (isBoardLocked) {
+        return;
+    }
+    //dening double click the same card
+    if (this == card1) {
         return;
     }
 
     if (isTimeGood) {
         this.classList.add('flip');
- 
-        if (!isCardFlipped) {
+
+        if (!isCardFlip) {
+            isCardFlip = true;
             card1 = this;
-            isCardFlipped = true;
-        } else {
-            card2 = this;
-            isCardFlipped = false;
+            return;
         }
-        matchingCardsFun();
+        card2 = this;
+        isCardFlip = false;
     }
+
+    //only if both got names then check match
+    if (name1 != null && name2 != null) {
+        CheckMatchCards();
+        name1 = null;
+        name2 = null;
+    }
+    TimerFun;
 }
 
+//---CHECK FOR MATCH CARDS FUNCTION---//
+var isCardsMatch = false;
 
-//----CHECK FOR MATCH CARDS FUNCTION-----//
-var cardCnt = 0;
-var tempPoints = 0;
-var corrctPairsCnt = 0;
+function CheckMatchCards() {
+    isBoardLocked = true;
+    TurnDetect();
 
-function matchingCardsFun() {
-    if (cardCnt == 2) {
-        if (cardName1 == cardName2) {
-            isCardsMath = true;
-            tempPoints++;
-            pointsPerPlayer();
-            corrctPairsCnt++;
-            card1.removeEventListener('click', flip);
-            card2.removeEventListener('click', flip);
-        } else {
-            isCardFaceUp = true;
-            isCardsMath = false;
-            setTimeout(f, 1000);
-            function f() {
-                isCardFaceUp = false;
-                card1.classList.remove('flip');
-                card2.classList.remove('flip');
-                turnsFun();
-            }
-        }
-        cardCnt = 0;
-        winner();
-    }
-}
-
-
-//----PLAYERS TURNS FUNCTION-----//
-var leftPlayerTurn = false
-var turnsCnt = 0;
-
-function turnDetect() {
-    if (!isCardsMath) {
-        turnsCnt += 0.5;
-    }
-
-    if (turnsCnt % 2 == 1) {
-        leftPlayerTurn = true;
+    if (name1 == name2) {
+        GotMatch();
     } else {
+        setTimeout(WorngCards, 1000);
+    }
+
+    GameScore();
+}
+
+//---IF CARDS IS MATCH FUNCTION---//
+var corrctPairsCnt = 0;
+function GotMatch() {
+    card1.removeEventListener('click', Flipper);
+    card2.removeEventListener('click', Flipper);
+    isCardsMatch = true;
+    isBoardLocked = false;
+    corrctPairsCnt++;
+    tempPoints++;
+    PlayersPoints();
+    tempPoints = 0;
+}
+
+//---IF CARDS DON'T MATCH FUNCTION---//
+function WorngCards() {
+    card1.classList.remove('flip');
+    card2.classList.remove('flip');
+    isBoardLocked = false;
+    isCardsMatch = false;
+
+    !leftPlayerTurn ? RightPlayerTurn() : LeftPlayerTurn();
+}
+
+
+/////*****---BONUS: TWO PLAYERS---******/////
+
+//---PLAYERS TURNS FUNCTION---//
+var leftPlayerTurn = false;
+var turnsCnt = 0;
+var leftplayerDiv = document.querySelector('.leftSide');
+var rightplayerDiv = document.querySelector('.rightSide');
+
+function TurnDetect() {
+    if (!isCardsMatch) turnsCnt++;
+
+    if (turnsCnt % 2 == 0) {
         leftPlayerTurn = false;
+        console.log(rightplayerDiv);
+    } else {
+        leftPlayerTurn = true;
+        console.log(leftplayerDiv);
     }
 }
 
-//----CARDS SHUFFLE FUNCTION-----//
-var cardPosition;
+//---TURNS STYLE FUNCTION---//
+var turnNoticeRight = document.getElementById('right-turn');
+turnNoticeRight.innerHTML = 'Your Turn';
+var turnNoticeLeft = document.getElementById('left-turn');
 
-(function shuffleCards() {
-    deckCard.forEach(j => {
-        cardPosition = Math.floor(Math.random() * 16);
-        j.style.order = cardPosition;
-    });
-})();
+function RightPlayerTurn() {
+    rightplayerDiv.classList.add('turn');
+    leftplayerDiv.classList.remove('turn');
+    turnNoticeRight.innerHTML = 'Your Turn'
+    turnNoticeLeft.innerHTML = ''
+    }
+
+function LeftPlayerTurn() {
+    leftplayerDiv.classList.add('turn');
+    rightplayerDiv.classList.remove('turn');
+    turnNoticeLeft.innerHTML = 'Your Turn'
+    turnNoticeRight.innerHTML = '';
+}
+
+
+//---POINTS FUNCTION---//
+var rightPointsDiv = document.getElementById('right-player-score');
+var leftPointsDiv = document.getElementById('left-player-score');
+var rightPlayerPoints = 0;
+var leftPlayerPoints = 0;
+var tempPoints = 0;
+rightPointsDiv.innerHTML = rightPlayerPoints;
+leftPointsDiv.innerHTML = leftPlayerPoints;
+
+function PlayersPoints() {
+    //if 'true' on purpose: it's still on right turn
+    if (leftPlayerTurn) {
+        rightPlayerPoints += tempPoints;
+        rightPointsDiv.innerHTML = rightPlayerPoints;
+    } else {
+        leftPlayerPoints += tempPoints;
+        leftPointsDiv.innerHTML = leftPlayerPoints;
+    }
+}
 
 
 //----TIMER FUNCTION-----//
-setInterval(timerFunc, 1000);
+setInterval(TimerFun, 1000);
+var isTimeGood = true;
 var maxTime = 1;
 var timeRemain = maxTime * 60;
-var isTimeGood = true;
 
-function timerFunc() {
+function TimerFun() {
     var countDown = document.getElementById('timer');
     var minutes = Math.floor(timeRemain / 60);
     var seconds = timeRemain % 60;
 
-    if (seconds < 10) {
-        seconds = '0' + seconds;
-    }
+    if (seconds < 10) seconds = '0' + seconds;
 
     if (corrctPairsCnt < 8) {
-        countDown.innerHTML = minutes + ' : ' + seconds;
+        countDown.innerHTML = '0' + minutes + ' : ' + seconds;
         timeRemain--;
     }
 
@@ -127,98 +179,39 @@ function timerFunc() {
         isTimeGood = false;
         countDown.innerHTML = '';
         countDown.innerHTML = "TIME IS OVER";
-        countDown.style.fontSize = "200%"
+        //countDown.style.fontSize = "2vh"
         countDown.style.letterSpacing = "0.2vw";
     }
-    winner();
+    GameScore();
 } 
 
+function GameScore() {
+    if (corrctPairsCnt == 8 || timeRemain < -1) {
+        var winnerIs = document.getElementById('winner');
+        winnerIs.innerHTML = '<div id="winner-div"></div>';
+        var container = document.getElementById('winner-div');
 
-//----TURNS STYLE FUNCTION-----//
-var rightPlayer = document.getElementById('right-side');
-var leftPlayer = document.getElementById('left-side');
-var turnNoticeRight = document.getElementById('right-turn');
-var turnNoticeLeft = document.getElementById('left-turn');
-turnNoticeRight.innerHTML = "Your Turn";
-rightPlayer.style.backgroundColor = "lightsteelblue";
-rightPlayer.style.opacity = "0.8";
-rightPlayer.style.boxShadow= "0 0 20px 10px #ffd800";
-
-function turnsFun() {
-    if (!leftPlayerTurn) {
-        turnNoticeLeft.innerHTML = "";
-        turnNoticeRight.innerHTML = "Your Turn";
-        leftPlayer.style.backgroundColor = "";
-        leftPlayer.style.boxShadow= "";
-        rightPlayer.style.boxShadow= "0 0 20px 10px #ffd800";
-        rightPlayer.style.backgroundColor = "lightsteelblue";
-        rightPlayer.style.opacity = "0.8";
-    } else {
-        turnNoticeRight.innerHTML = "";
-        turnNoticeLeft.innerHTML = "Your Turn";
-        rightPlayer.style.backgroundColor = "";
-        rightPlayer.style.boxShadow= "";
-        leftPlayer.style.boxShadow= "0 0 20px 10px blue";
-        leftPlayer.style.backgroundColor = "lightsteelblue";
-        leftPlayer.style.opacity = "0.8";
-    }
-}
-
-
-//----PLAYERS POINTS FUNCTION-----//
-var rightPlayerPoints = document.getElementById('right-player-score');
-var leftPlayerPoints = document.getElementById('left-player-score');
-var player1Points = 0;
-var player2Points = 0
-
-function pointsPerPlayer() {
-    if (!leftPlayerTurn) {
-        player2Points += tempPoints;
-        leftPlayerPoints.innerHTML = player2Points;
-    } else {
-        player1Points += tempPoints;
-        rightPlayerPoints.innerHTML = player1Points;
-    }
-    tempPoints = 0
-
-}
-
-
-//----WINNER FUNCTION-----//
-function winner() {
-    console.log(player1Points, player2Points);
-    if (corrctPairsCnt == 8 || timeRemain < 0) {
-        var winnerIs = document.getElementById('middle');
-        var div = document.createElement('div');
         var h = document.createElement("h1");
-        var h1 = document.createElement("h1");
-        var node = document.createTextNode("The Winner Is:");
-        var node1 = document.createTextNode("PLAYER 1!!");
-        var node2 = document.createTextNode("PLAYER 2!!");
-        var node3 = document.createTextNode("IT'S A DRAW");
-        h.appendChild(node);
-        div.appendChild(h);
-        //div.style.zIndex = '1';
-        div.style.position = 'absolute';
-        div.style.opacity = '0.2';
-        div.style.top = '0';
-        div.style.backgroundColor = 'black';
-        div.style.color = 'white';
-        div.style.fontSize = '180%';
-        div.style.width = '100%';
-        div.style.height = '100%';
-        div.style.textAlign = 'center';
-        div.style.borderRadius = '5px';
+        h.innerHTML = "The Winner Is:";
+        container.appendChild(h);
 
-        if (player1Points > player2Points) {
-            h1.appendChild(node1);
-        } else if (player1Points < player2Points) {
-            h1.appendChild(node2);
+        var h1 = document.createElement("h1");
+        if (rightPlayerPoints > leftPlayerPoints) {
+            h1.innerHTML = "PLAYER 1!!";
+            container.style.color = "#ffd800";
+            container.style.boxShadow = "0 0 10px 3px #ffd800";
+        } else if (rightPlayerPoints < leftPlayerPoints) {
+            h1.innerHTML = "PLAYER 2!!";
+            container.style.color = "#125ce3";
+            container.style.boxShadow = "0 0 10px 3px blue";
         } else {
-            h1.appendChild(node3);
+            h.innerHTML = "";
+            container.style.color = "red";
+            h1.innerHTML = "IT'S A DRAW";
+            container.style.boxShadow = "0 0 10px 3px red";
         }
-        div.appendChild(h1);        
-        winnerIs.appendChild(div);
+        container.style.zIndex = '1';
+        container.appendChild(h1);
+        winnerIs.appendChild(container);
     } 
 }
-
